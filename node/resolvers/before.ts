f5d1d266b8d0c5536f7f459e09334a0f84a60eef
Hiago Lucas Cardeal de Melo Silva <hcardealdemelo@gmail.com>
2022-07-18 15:37:52 -0300

    change before example to follow the documentation

--- node/resolvers/before.ts
+++ node/resolvers/before.ts
@@ -5,18 +5,17 @@ export default async function before(
 ): Promise<SearchParams> {
   // This is just an example. Change the implementation of this function however you like :)
 
-  const myDynamicRules: DynamicRule[] = [
-    {
-      action: 'promote',
-      type: 'id',
-      value: '13',
-    },
-    {
-      action: 'promote',
-      type: 'id',
-      value: '2000024',
-    },
-  ]
+  if (args.query === 'shoes') {
+    const myDynamicRules: DynamicRule[] = [
+      {
+        action: 'promote',
+        type: 'id',
+        value: '123',
+      },
+    ]
 
-  return { ...args, query: 'top', dynamicRules: myDynamicRules }
+    return { ...args, dynamicRules: myDynamicRules }
+  }
+
+  return args
 }
