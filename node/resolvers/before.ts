f4fedb25e5a08e6f1777617e579bf71752727eff
edyespinal <edy.espinal@vtex.com.br>
2022-11-04 12:06:01 +0100

    chore: refactor to improve error handling

--- node/resolvers/before.ts
+++ node/resolvers/before.ts
@@ -10,61 +10,65 @@ export async function before(
     vtex: { logger },
   } = ctx
 
-  try {
-    const searchQuery = args.query
+  const searchQuery = args.query
 
-    if (!searchQuery) {
-      throw new Error('Query was undefined')
-    }
+  if (!searchQuery) {
+    const message = 'Query was undefined'
 
-    const settings: AppSettings = await apps.getAppSettings(
-      process.env.VTEX_APP_ID as string
-    )
-
-    if (!Object.keys(settings).length) {
-      throw new Error('Settings not found')
-    }
-
-    const {
-      adServerName,
-      boostType,
-      maxOffersToDisplay,
-      addAllProducts,
-      production,
-    } = settings
-
-    const { offers } = await relevanC.getSponsoredOffers(
-      production,
-      adServerName,
-      {
-        sourcePageNumber: 0,
-        keyOrigin: searchQuery,
-        adSpaceId: DESKTOP,
-      }
-    )
-
-    if (!offers.length) {
-      throw new Error('No offers returned from RelevanC')
-    }
-
-    const dynamicRules = offers.reduce((rules: DynamicRule[], offer, index) => {
-      if (index < maxOffersToDisplay) {
-        rules.push({
-          action: addAllProducts ? ACTION.ADD : ACTION.PROMOTE,
-          type: boostType,
-          value: offer.productId,
-        })
-      }
-
-      return rules
-    }, [])
-
-    return { ...args, dynamicRules }
-  } catch (error) {
     logger.error({
-      message: error.message ?? 'Something went wrong',
+      message,
     })
 
-    return error
+    throw new Error(message)
   }
+
+  const settings: AppSettings = await apps.getAppSettings(
+    process.env.VTEX_APP_ID as string
+  )
+
+  if (!Object.keys(settings).length) {
+    const message = 'Settings for RelevanC Integration not found'
+
+    logger.error({
+      message,
+    })
+
+    throw new Error(message)
+  }
+
+  const {
+    adServerName,
+    boostType,
+    maxOffersToDisplay,
+    addAllProducts,
+    production,
+  } = settings
+
+  const { offers } = await relevanC.getSponsoredOffers(
+    production,
+    adServerName,
+    {
+      sourcePageNumber: 0,
+      keyOrigin: searchQuery,
+      adSpaceId: DESKTOP,
+    }
+  )
+
+  if (!offers.length) {
+    return args
+  }
+
+  const dynamicRules = offers.reduce((rules: DynamicRule[], offer, index) => {
+    if (index < maxOffersToDisplay) {
+      rules.push({
+        action: addAllProducts ? ACTION.ADD : ACTION.PROMOTE,
+        type: boostType,
+        value: offer.productId,
+      })
+    }
+
+    return rules
+  }, [])
+
+  return { ...args, dynamicRules }
 }
