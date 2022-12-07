276920053203fcb9ba8472733cda232d14c009ad
edyespinal <edy.espinal@vtex.com.br>
2022-12-07 16:40:05 +0100

    chore: refactor relevanc client

--- node/clients/relevanC.ts
+++ node/clients/relevanC.ts
@@ -1,31 +1,32 @@
-import { InstanceOptions, IOContext, ExternalClient } from '@vtex/api'
-
-const BASE_URL = (name: string) => ({
-  STAGING: `http://${name}.staging-ads.peps.relevanc.io`,
-  PRODUCTION: `http://${name}.ads.peps.relevanc.io`,
-})
+import {
+  InstanceOptions,
+  IOContext,
+  ExternalClient,
+  PRODUCTION,
+} from '@vtex/api'
 
 export default class RelevanC extends ExternalClient {
   constructor(context: IOContext, options?: InstanceOptions) {
     super('https://ads/peps.relevanc.io', context, options)
   }
 
+  private getBaseUrl(serverName: string) {
+    return PRODUCTION
+      ? `http://${serverName}.ads.peps.relevanc.io`
+      : `http://${serverName}.staging-ads.peps.relevanc.io`
+  }
+
   public async getSponsoredOffers(
-    production: boolean,
     serverName: string,
     params: SponsoredProductsPayload
   ): Promise<SponsoredProductsResponse> {
-    const URL = production
-      ? BASE_URL(serverName).PRODUCTION
-      : BASE_URL(serverName).STAGING
+    const URL = this.getBaseUrl(serverName)
 
     return this.http.get(`${URL}/sponsored-offers`, {
       headers: {
         'x-vtex-use-https': 'true',
       },
-      params: {
-        ...params,
-      },
+      params,
       metric: 'relevanc-get-offers',
     })
   }
