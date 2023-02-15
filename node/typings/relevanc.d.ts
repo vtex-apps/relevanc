4a5791462035aec3ea6c761e828231e1020b1b82
edyespinal <edy.espinal@vtex.com.br>
2023-02-15 14:22:51 +0100

    chore: add Relevanc namespace

--- node/typings/relevanc.d.ts
+++ node/typings/relevanc.d.ts
@@ -1,49 +1,52 @@
-type BaseSponsoredProductsPaylaod = {
-  adSpaceId: 'PC_LR_PLR' | 'MOB_LR_PLR'
-  sourcePageNumber: number
-}
+declare namespace Relevanc {
+  type BaseSponsoredProductsPaylaod = {
+    adSpaceId: 'PC_LR_PLR' | 'MOB_LR_PLR'
+    sourcePageNumber: number
+  }
 
-type KeyIdSponsoredProductsPayload = BaseSponsoredProductsPaylaod & {
-  keyId: string
-  filterCategoryCodePath: string
-}
+  type KeyIdSponsoredProductsPayload = BaseSponsoredProductsPaylaod & {
+    keyId: string
+    filterCategoryCodePath: string
+  }
 
-type KeyOriginSponsoredProductsPayload = BaseSponsoredProductsPaylaod & {
-  keyOrigin: string
-  filterCategoryCodePath?: string
-}
+  type KeyOriginSponsoredProductsPayload = BaseSponsoredProductsPaylaod & {
+    keyOrigin: string
+    filterCategoryCodePath?: string
+  }
 
-type SponsoredProductsPayload =
-  | KeyIdSponsoredProductsPayload
-  | KeyOriginSponsoredProductsPayload
+  type SponsoredProductsPayload =
+    | KeyIdSponsoredProductsPayload
+    | KeyOriginSponsoredProductsPayload
 
-type SponsoredOffer = {
-  algoSelectionType:
-    | 'NOTSELECTED'
-    | 'EXPLORED'
-    | 'UNEXPLORED'
-    | 'LONGTAIL'
-    | 'SHORTTAIL'
-  isSponsored: boolean
-  offerId: number
-  productId: string
-  tagName: string
-  tag: string
-}
+  type SponsoredOffer = {
+    algoSelectionType:
+      | 'NOTSELECTED'
+      | 'EXPLORED'
+      | 'UNEXPLORED'
+      | 'LONGTAIL'
+      | 'SHORTTAIL'
+    isSponsored: boolean
+    offerId: number
+    productId: string
+    tagName: string
+    tag: string
+  }
 
-type SponsoredProductsResponse = {
-  nbResults: number
-  tagName: string
-  tag: string
-  offers: SponsoredOffer[]
-}
+  type SponsoredProductsResponse = {
+    nbResults: number
+    tagName: string
+    tag: string
+    offers: SponsoredOffer[]
+  }
 
-interface HandlerArgs {
-  ctx: Context
-  settings: AppSettings
-  searchParams: SearchParams
-}
+  interface HandlerArgs {
+    ctx: Context
+    settings: AppSettings
+    searchParams: SearchParams
+  }
 
-interface SponsoredOffersMap {
-  [offerId: string]: SponsoredOffer
+  interface SponsoredOffersMap {
+    boostType: BoostType
+    [offerId: string]: SponsoredOffer
+  }
 }
