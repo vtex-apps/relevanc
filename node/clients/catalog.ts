b019ede42b0788ed443d6b9f3bb2ec382b26afb8
edyespinal <edy.espinal@vtex.com.br>
2023-02-15 14:31:35 +0100

    chore: update catalog client

--- node/clients/catalog.ts
+++ node/clients/catalog.ts
@@ -2,10 +2,18 @@ import { JanusClient } from '@vtex/api'
 
 export default class Catalog extends JanusClient {
   public getCategoryTree = async (
+    /**
+     * We use a high number to be sure to get all levels in category tree
+     * @see https://developers.vtex.com/docs/api-reference/catalog-api#get-/api/catalog_system/pub/category/tree/-categoryLevels-
+     */
+
     categoryLevels = 99
   ): Promise<CategoryTree> => {
     return this.http.get(
-      `/api/catalog_system/pub/category/tree/${categoryLevels}`
+      `/api/catalog_system/pub/category/tree/${categoryLevels}`,
+      {
+        metric: 'catalog-get-category-tree',
+      }
     )
   }
 }
