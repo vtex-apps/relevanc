73f822ecb16782ffd68c8cad7c820a94124e3cc1
edyespinal <edy.espinal@vtex.com.br>
2023-02-15 14:23:34 +0100

    chore: update categoriesMap client

--- node/clients/categories.ts
+++
@@ -1,7 +0,0 @@
-import { vbaseFor } from '@vtex/clients'
-
-import { BUCKET } from '../contants'
-
-const Categories = vbaseFor<string, CategoriesMapRecord>(BUCKET)
-
-export default Categories
---
+++ node/clients/categoriesMap.ts
@@ -0,0 +1,16 @@
+import { InstanceOptions, IOContext, VBase } from '@vtex/api'
+import { BUCKET, CATEGORIES_MAP } from '../contants'
+
+export default class CategoriesMap extends VBase {
+  constructor(context: IOContext, options?: InstanceOptions) {
+    super(context, {
+      ...options,
+    })
+  }
+
+  public getCategoriesMap = (): Promise<CategoriesMapRecord | null> =>
+    this.getJSON(BUCKET, CATEGORIES_MAP, true)
+
+  public updateCategoriesMap = (data: CategoriesMapRecord) =>
+    this.saveJSON(BUCKET, CATEGORIES_MAP, data)
+}
